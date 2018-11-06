68cb077cc6d26028fa9fe7b51693bcdeb5744940
Luke Hollis <lukehollis@gmail.com>
2018-11-05 23:22:13 -0500

    add person project meta fields to info modal content

--- src/modules/mindthegap/components/InfoModalContent/InfoModalContent.js
+++ src/modules/mindthegap/components/InfoModalContent/InfoModalContent.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
 import pluralize from 'pluralize';
 
 // components
+import Tags from '../../../tags/components/Tags';
+import ItemTitle from '../../../items/components/ItemTitle';
 import ItemImageViewer from '../../../items/components/ItemImageViewer';
+import ItemDescription from '../../../items/components/ItemDescription';
+import ItemMetaFields from '../../../items/components/ItemMetaFields';
+import PersonItemMetaFieldItemsContainer from '../../../people/containers/PersonItemMetaFieldItemsContainer';
+
 
 import './InfoModalContent.css';
 
 
 const InfoModalContent = props => {
+	console.log(props);
+	console.log(props);
+	console.log(props);
+	console.log(props);
 
 	return (
 		<div className="infoModalContent">
 
 			<div className="infoModalTitle">
-				<h1>{props.title}</h1>
+				<ItemTitle
+					_id={props._id}
+					title={props.name || props.title}
+					editLink={props.userIsAdmin ? `/people/${props._id}/${props.slug}/edit` : null}
+					handleRemove={props.userIsAdmin ? props.handleRemove : null}
+				/>
 			</div>
 			<div className="infoModalMedia">
 				{props.files ?
@@ -25,138 +39,37 @@ const InfoModalContent = props => {
 				: ''}
 			</div>
 			<div className="infoModalBody">
-				<Row>
-					<Col md={8}>
-						<div className="infoModalContentText">
-							<p>
-								{props.description}
-							</p>
-						</div>
-					</Col>
-					<Col md={4}>
-						{/*
-						<div className="infoModalContentDetails">
-							<aside className="personAside withBorder">
-								<div
-									className="personAsideThumbnail"
-									style={{
-										backgroundImage: 'url(/images/example1.jpg)',
-										backgroundSize: 'cover',
-										backgroundPosition: 'center',
-										backgroundRepeat: 'no-repeat',
-										height: '70px',
-										width: '70px',
-									}}
-	              />
-								<div className="personAsideText">
-									<h4>Juan Bosch</h4>
-									<p>Yolanda: I went once on vacation to Barahona . . .</p>
-								</div>
-							</aside>
-							<aside className="personAside">
-								<div
-									className="personAsideThumbnail"
-									style={{
-										backgroundImage: 'url(/images/example2.jpg)',
-										backgroundSize: 'cover',
-										backgroundPosition: 'center',
-										backgroundRepeat: 'no-repeat',
-										height: '70px',
-										width: '70px',
-									}}
-	              />
-								<div className="personAsideText">
-									<h4>Piky Lora</h4>
-									<p>Yolanda: I went once on vacation to Barahona . . .</p>
-								</div>
-							</aside>
-							<aside className="personAside">
-								<div
-									className="personAsideThumbnail"
-									style={{
-										backgroundImage: 'url(/images/example3.jpg)',
-										backgroundSize: 'cover',
-										backgroundPosition: 'center',
-										backgroundRepeat: 'no-repeat',
-										height: '70px',
-										width: '70px',
-									}}
-	              />
-								<div className="personAsideText">
-									<h4>Teresa Espaillant</h4>
-									<p>Yoland: I went once on vacation to Barahona . . .</p>
-								</div>
-							</aside>
-						</div>
-						*/}
-					</Col>
-				</Row>
-			</div>
-			{/*
-			<div className="infoModalSimilar">
-				<h2>Items Similar to this Post</h2>
-				<div className="itemRelatedList">
-					<div className="itemRelatedTeaser">
-						<div
-							className="itemRelatedTeaserThumbnail"
-							style={{
-	  						backgroundImage: 'url(/images/example4.jpg)',
-	  						backgroundSize: 'cover',
-	  						backgroundPosition: 'center',
-	  						backgroundRepeat: 'no-repeat',
-	  						height: '300px',
-	  						width: '320px',
-	  					}}
-	          />
-						<h4 className="itemRelatedTeaserTitle">
-	            1965 Immigration Act
-						</h4>
-						<label className="itemRelatedTeaserLabel">
-	            Santo Domingo, Dominican Republic
-						</label>
-					</div>
-					<div className="itemRelatedTeaser">
-						<div
-							className="itemRelatedTeaserThumbnail"
-							style={{
-	  						backgroundImage: 'url(/images/example7.jpg)',
-	  						backgroundSize: 'cover',
-	  						backgroundPosition: 'center',
-	  						backgroundRepeat: 'no-repeat',
-	  						height: '300px',
-	  						width: '320px',
-	  					}}
-	          />
-						<h4 className="itemRelatedTeaserTitle">
-	            La Guerra de Abril
-						</h4>
-						<label className="itemRelatedTeaserLabel">
-	            Santo Domingo, Dominican Republic
-						</label>
-					</div>
-					<div className="itemRelatedTeaser">
-						<div
-							className="itemRelatedTeaserThumbnail"
-							style={{
-	  						backgroundImage: 'url(/images/example6.jpg)',
-	  						backgroundSize: 'cover',
-	  						backgroundPosition: 'center',
-	  						backgroundRepeat: 'no-repeat',
-	  						height: '300px',
-	  						width: '320px',
-	  					}}
-	          />
-						<h4 className="itemRelatedTeaserTitle">
-	            Transnational Commemoration
-						</h4>
-						<label className="itemRelatedTeaserLabel">
-	            Santo Domingo, Dominican Republic
-						</label>
-					</div>
+				<ItemDescription
+					description={props.bio}
+				/>
 
-				</div>
+				<Tags
+					tags={props.tags}
+				/>
+
+				<ItemMetaFields
+					metafields={props.metadata}
+				/>
+
+				<PersonItemMetaFieldItemsContainer
+					label="Interviews"
+					ids={props.interviews}
+					type="interview"
+				/>
+
+				<PersonItemMetaFieldItemsContainer
+					label="Events"
+					ids={props.events}
+					type="event"
+				/>
+
+				<PersonItemMetaFieldItemsContainer
+					label="Items"
+					ids={props.items}
+					type="item"
+			/>
+
 			</div>
-			*/}
 		</div>
 	);
 }
